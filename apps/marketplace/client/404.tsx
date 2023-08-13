132d145db5024be17362091a5c5ee4f88968f4d3
gubmax <gubyshev@gmail.com>
2023-08-13 12:20:25 +0300

    enh(merketplace): update 404 layout

--- apps/marketplace/client/404.tsx
+++ apps/marketplace/client/404.tsx
@@ -1,12 +1,13 @@
 import BaseLayout from './common/components/layouts/base/base-layout.js'
+import NotFound from './common/components/not-found/not-found.js'
 import type { MetaFunction } from './core/components/page.js'
 
 export const meta: MetaFunction = () => ({ title: 'Page not found' })
 
 export default function NotFoundPage() {
 	return (
-		<BaseLayout>
-			<p>Not found</p>
+		<BaseLayout className="flex justify-center flex-col">
+			<NotFound />
 		</BaseLayout>
 	)
 }
--- apps/marketplace/client/common/components/layouts/base/base-layout.tsx
+++ apps/marketplace/client/common/components/layouts/base/base-layout.tsx
@@ -1,17 +1,21 @@
+import { cn } from 'ui/helpers/class-names.js'
 import type { ChildrenProp } from 'ui/typings/children-prop.js'
+import type { StyledProps } from 'ui/typings/styled-props.js'
 
 import Footer from './footer/footer.js'
 import Header from './header/header.js'
 import Sidebar from './sidebar/sidebar.js'
 import './base-layout.css'
 
-function BaseLayout({ children }: ChildrenProp) {
+function BaseLayout({ className, style, children }: ChildrenProp & StyledProps) {
 	return (
 		<div className="m-base-layout">
 			<Sidebar />
-			<div className="w-full">
+			<div className="w-full" style={style}>
 				<Header />
-				<main className="m-base-layout__main mx-auto px-10 pt-22 pb-20">{children}</main>
+				<main className={cn('m-base-layout__main mx-auto px-10 pt-22 pb-20', className)}>
+					{children}
+				</main>
 				<Footer />
 			</div>
 		</div>
