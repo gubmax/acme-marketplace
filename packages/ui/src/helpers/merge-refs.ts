fbfc8d1300a70487b6ad0b51bb17d829e8e3f28b
gubmax <gubyshev@gmail.com>
2024-01-07 12:33:47 +0300

    docs(ui): add desc for merge-refs fn

--- packages/ui/src/helpers/merge-refs.ts
+++ packages/ui/src/helpers/merge-refs.ts
@@ -1,8 +1,10 @@
 import type { LegacyRef, MutableRefObject, RefCallback } from 'react'
 
-export function mergeRefs<T = unknown>(
-	...refs: Array<MutableRefObject<T> | LegacyRef<T>>
-): RefCallback<T> {
+/**
+ * Merges multiple refs into a single function ref.
+ * Refs can be mutable refs or function refs.
+ */
+export function mergeRefs<T>(...refs: Array<MutableRefObject<T> | LegacyRef<T>>): RefCallback<T> {
 	return (value) => {
 		for (const ref of refs) {
 			if (typeof ref === 'function') ref(value)
